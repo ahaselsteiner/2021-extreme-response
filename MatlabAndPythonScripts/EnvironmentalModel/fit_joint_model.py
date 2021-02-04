311843bc3d2172cda43753654de5d2980ee7b5df
Andreas Haselsteiner <Admin@IKAP-TOP48.biba.uni-bremen.de>
2021-02-04 17:52:02 +0100

    Define simluation points for tp and create a figure that shows them.

--- MatlabAndPythonScripts/EnvironmentalModel/fit_joint_model.py
+++ MatlabAndPythonScripts/EnvironmentalModel/fit_joint_model.py
@@ -3,6 +3,7 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.patches import Polygon
 import csv, math
 
 from viroconcom.fitting import Fit
@@ -63,6 +64,44 @@ axs[1].set_xlabel(tp_label)
 axs[1].spines['right'].set_visible(False)
 axs[1].spines['top'].set_visible(False)
 fig_rawdata.savefig('gfx/EnvironmentalDataset.pdf', bbox_inches='tight')
+
+fig_sim_points, axs = plt.subplots(1, 2, figsize=(9,4), dpi=300, sharey=True)
+inc = 2
+hs_sim = np.arange(1, 15 + inc, inc)
+inc = 2
+v_sim = np.arange(1, 25 + inc, inc)
+v_sim = np.append(v_sim, np.array([26, 30, 35, 40, 45]))
+vgrid, hsgrid = np.meshgrid(v_sim, hs_sim)
+axs[0].scatter(v, hs, c='black', s=5, alpha=0.5, rasterized=True)
+axs[0].scatter(vgrid, hsgrid, c='red', s=10)
+
+verts = [(0, 8), (8, 8), (8, 10), (16, 10), (16, 12), (20, 12), (20, 15.5), (0, 15.5)]
+poly = Polygon(verts, facecolor='0.9', edgecolor='0.5')
+axs[0].add_patch(poly)
+
+axs[0].set_xlabel(v_label)
+axs[0].set_ylabel(hs_label)
+axs[0].spines['right'].set_visible(False)
+axs[0].spines['top'].set_visible(False)
+axs[1].scatter(tp, hs, c='black', s=5, alpha=0.5, rasterized=True)
+hs_s = np.arange(0.1, 15, 0.2)
+sps = [1/15, 1/20]
+for i in range(2):
+    sp = sps[i]
+    tp_s = np.sqrt((2 * math.pi * hs_s) / (g * sp))
+    axs[1].plot(tp_s, hs_s, c='blue', zorder=9)
+    tp_sim = np.sqrt((2 * math.pi * hs_sim) / (g * sp))
+    axs[1].scatter(tp_sim, hs_sim, c='red', s=10, zorder=10)
+add_to_tp = [8, 20]
+for i in range(2):
+    axs[1].plot(tp_s + 1 / (1 + np.power(hs_s + 2, 0.5)) * add_to_tp[i], hs_s, c='blue', zorder=9)
+    axs[1].scatter(tp_sim + 1 / (1 + np.power(hs_sim + 2, 0.5)) * add_to_tp[i], hs_sim, c='red', s=10, zorder=10)
+
+axs[1].set_xlabel(tp_label)
+axs[1].spines['right'].set_visible(False)
+axs[1].spines['top'].set_visible(False)
+fig_sim_points.savefig('gfx/SimulationPoints.pdf', bbox_inches='tight')
+
 
 fig_steepness, axs = plt.subplots(1,3, figsize=(13,4), dpi=300)
 axs[0].scatter(hs, steepness, c='black', s=5, alpha=0.5, rasterized=True)
