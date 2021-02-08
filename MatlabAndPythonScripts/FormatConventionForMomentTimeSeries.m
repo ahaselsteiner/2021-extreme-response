2f3be11a5d0f082d58af37534d9e422f468c84e5
Andreas Haselsteiner <Admin@IKAP-TOP48.biba.uni-bremen.de>
2021-02-08 17:58:31 +0100

    Add comments to clarify where format is specified.

--- MatlabAndPythonScripts/FormatConventionForMomentTimeSeries.m
+++ MatlabAndPythonScripts/FormatConventionForMomentTimeSeries.m
@@ -1,3 +1,10 @@
+% ** Explanation about the file format: **
+%
+% 1. dimension = wind, 2. dimension = hs, 3. dimension = tp
+M(1,3,4,:) = 'Response time series (as a double vector)';
+% v(1), hs(3), tp(hs(3), 4)
+% v = 1, hs = 5, tp = 13.4889
+
 v = [1:2:25, 26, 30, 35, 40, 45];
 hs = [1:2:15];
 y = @(x) x.^a;
@@ -9,10 +16,9 @@ tp3 = @(hs) sqrt(2 * pi * hs / (9.81 * 1/20)) + 1 ./ (1 + sqrt(hs + 2)) * 8;
 tp4 = @(hs) sqrt(2 * pi * hs / (9.81 * 1/20)) + 1 ./ (1 + sqrt(hs + 2)) * 20;
 tp = @(hs, idx) (idx == 1) .* tp1(hs) + (idx == 2) .* tp2(hs) + (idx ==3) .* tp3(hs) + (idx ==4) .* tp4(hs);
 
-% 1. dimension = wind, 2. dimension = hs, 3. dimension = tp
-M(1,3,4,:) = 'Response time series (as a double vector)';
-% v(1), hs(3), tp(hs(3), 4)
-% v = 1, hs = 5, tp = 13.4889
+% ** End of explanation. **
+
+
 
 if exist ('DataEmulator.mat')
     load 'DataEmulator.mat';
