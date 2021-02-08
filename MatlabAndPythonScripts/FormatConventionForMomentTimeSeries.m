310c6ef977a81dc5fc5d72c94176ce3a0c614180
Andreas Haselsteiner <Admin@IKAP-TOP48.biba.uni-bremen.de>
2021-02-08 17:57:04 +0100

    Clarify format.

--- MatlabAndPythonScripts/FormatConventionForMomentTimeSeries.m
+++ MatlabAndPythonScripts/FormatConventionForMomentTimeSeries.m
@@ -1,4 +1,4 @@
-v = [3:2:25, 26, 30, 35, 40, 45];
+v = [1:2:25, 26, 30, 35, 40, 45];
 hs = [1:2:15];
 y = @(x) x.^a;
 % S_p = (2 pi H_s) / (g T_p^2)
@@ -7,7 +7,12 @@ tp1 = @(hs) sqrt(2 * pi * hs / (9.81 * 1/15));
 tp2 = @(hs) sqrt(2 * pi * hs / (9.81 * 1/20));
 tp3 = @(hs) sqrt(2 * pi * hs / (9.81 * 1/20)) + 1 ./ (1 + sqrt(hs + 2)) * 8;
 tp4 = @(hs) sqrt(2 * pi * hs / (9.81 * 1/20)) + 1 ./ (1 + sqrt(hs + 2)) * 20;
-tp = [tp1(hs), tp2(hs), tp3(hs), tp4(hs)];
+tp = @(hs, idx) (idx == 1) .* tp1(hs) + (idx == 2) .* tp2(hs) + (idx ==3) .* tp3(hs) + (idx ==4) .* tp4(hs);
+
+% 1. dimension = wind, 2. dimension = hs, 3. dimension = tp
+M(1,3,4,:) = 'Response time series (as a double vector)';
+% v(1), hs(3), tp(hs(3), 4)
+% v = 1, hs = 5, tp = 13.4889
 
 if exist ('DataEmulator.mat')
     load 'DataEmulator.mat';
