4bea4899bfd87b15c53b2768230a2ad0bec7adcd
Andreas Haselsteiner <Admin@IKAP-TOP48.biba.uni-bremen.de>
2021-02-11 22:01:55 +0100

    Improve comments on file format.

--- MatlabAndPythonScripts/FormatConventionForMomentTimeSeries.m
+++ MatlabAndPythonScripts/FormatConventionForMomentTimeSeries.m
@@ -1,15 +1,14 @@
 % ** Explanation about the file format: **
 %
-% 1. dimension = wind, 2. dimension = hs, 3. dimension = tp
+% 1. dimension = wind, 2. dim = hs, 3. dim = tp, 4. dim = response
 M(1,3,4,:) = 'Response time series (as a single vector)';
 % v(1), hs(3), tp(hs(3), 4)
 % v = 1, hs = 5, tp = 13.4889
 
 v = [1:2:25, 26, 30, 35, 40, 45];
 hs = [1:2:15];
-y = @(x) x.^a;
 
-% S_p = (2 pi H_s) / (g T_p^2)
+% The peak steepness S_p = (2 pi H_s) / (g T_p^2)
 % --> tp = sqrt(2 * pi * hs / (g * s_p))
 tp1 = @(hs) sqrt(2 * pi * hs / (9.81 * 1/15));
 tp2 = @(hs) sqrt(2 * pi * hs / (9.81 * 1/20));
