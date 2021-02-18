b49823a2e611aee587a091f34208fd781a2fe94d
Andreas Haselsteiner <Admin@IKAP-TOP48.biba.uni-bremen.de>
2021-02-18 11:33:50 +0100

    Do not give Tp value if Hs = 0.

--- MatlabAndPythonScripts/ResponseEmulator/GevAnalysisAtAllWindSpeeds.m
+++ MatlabAndPythonScripts/ResponseEmulator/GevAnalysisAtAllWindSpeeds.m
@@ -11,24 +11,29 @@ for vid = 1 : size(Ovr, 1)
             r = Ovr(vid, hsid, tpid, :);
             [pd, block_maxima] = gevToBlockMaxima(r, 60, 'free');
             ks(vid, hsid, tpid) = pd.k;
-            if hsid == 1 && tpid == 1
+            if hsid == 2 && tpid == 1
                 nexttile
                 h = qqplot(block_maxima, pd);
                 title(['v = ' num2str(v(vid)) ' m/s, k = ' num2str(pd.k)])
             end
         end
-    ende
+    end
 end
 
 ks(ks == 0) = nan;
 figure
-t = tiledlayout(2, 1);
-for tpid = 1 : 2
+t = tiledlayout(2, 2);
+for tpid = 1 : 4
     nexttile
     hold on
     for hsid = 1 : size(ks, 2)
-        h = plot(v, ks(:, hsid, tpid), 'linewidth', 2, ...
-            'DisplayName', ['H_s = ' num2str(hs(hsid)) ' m, T_p = ' num2str(tp(hs(hsid), tpid))]);
+        if hsid == 1
+            h = plot(v, ks(:, hsid, tpid), 'linewidth', 2, ...
+                'DisplayName', ['H_s = ' num2str(hs(hsid)) ' m']);
+        else
+            h = plot(v, ks(:, hsid, tpid), 'linewidth', 2, ...
+                'DisplayName', ['H_s = ' num2str(hs(hsid)) ' m, T_p = ' num2str(tp(hs(hsid), tpid))]);
+        end
     end
     h = plot(v, mean(ks(:, :, tpid), 2, 'omitnan'), '--k', 'DisplayName', 'Mean');
     xlabel('1-hour wind speed (m/s)');
