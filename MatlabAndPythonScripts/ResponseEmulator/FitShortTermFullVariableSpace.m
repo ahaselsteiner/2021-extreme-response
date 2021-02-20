b965e2d939610d8c48ee036c17744c675d51a160
Andreas Haselsteiner <Admin@IKAP-TOP48.biba.uni-bremen.de>
2021-02-20 03:45:03 +0100

    Add visualization.

--- MatlabAndPythonScripts/ResponseEmulator/FitShortTermFullVariableSpace.m
+++ MatlabAndPythonScripts/ResponseEmulator/FitShortTermFullVariableSpace.m
@@ -7,11 +7,10 @@ gridSize = size(maxr);
 currentNrEntries = sum(sum(sum(maxr>0)))
 
 % Checking whether the results make sense:
-vid = 1;
-hsid = 3;
+vid = 14;
+hsid = 2;
 figure('Position', [100 100 900 900])
-subplot(4, 1, 1:2);
-addpath('03_Calm_Sea_Complete_Wind')
+subplot(5, 1, 1:2);
 load 'CalmSeaComplete.mat';
 OvrAllSeeds = [Ovr_S1; Ovr_S2; Ovr_S3; Ovr_S4; Ovr_S5; Ovr_S6];
 OvrAllSeeds = OvrAllSeeds(:, 1:18);
@@ -31,29 +30,35 @@ h = scatter(v, maxr(:, hsid, 1), ms, 'MarkerFaceColor', 'red', ...
     'MarkerFaceAlpha', 0.5, 'MarkerEdgeColor', 'k', 'DisplayName', ['Hs = ' num2str(hs(hsid)) ' m, Tp = ' num2str(tp(hs(hsid), 1))]);
 h = scatter(v, maxr(:, hsid, 2), ms, 'MarkerFaceColor', 'blue', ...
     'MarkerFaceAlpha', 0.5, 'MarkerEdgeColor', 'k', 'DisplayName', ['Hs = ' num2str(hs(hsid)) ' m, Tp = ' num2str(tp(hs(hsid), 2))]);
+h = scatter(v, maxr(:, hsid, 3), ms, 'MarkerFaceColor', 'green', ...
+    'MarkerFaceAlpha', 0.5, 'MarkerEdgeColor', 'k', 'DisplayName', ['Hs = ' num2str(hs(hsid)) ' m, Tp = ' num2str(tp(hs(hsid), 3))]);
 legend('box', 'off', 'location', 'eastoutside')
 xlabel('1-hr wind speed (m/s)');
 ylabel('Max 1-hr overturning moment (Nm)');
 
-subplot(4, 1, 3);
+subplot(5, 1, 3);
 
 plot(minutes(Time/60), squeeze(Ovr(vid, hsid, 1, :)))
-%xlabel('Time step (-)');
 ylabel('Overturning moment (Nm)');
 box off
 legend(['V = ' num2str(v(vid)) ' m/s, Hs = ' num2str(hs(hsid)) ' m, Tp = ' num2str(tp(hs(hsid), 1))], 'box', 'off', 'location', 'eastoutside');
 
-subplot(4, 1, 4);
+subplot(5, 1, 4);
 plot(minutes(Time/60), squeeze(Ovr(vid, hsid, 2, :)))
-%xlabel('Time step (-)');
 ylabel('Overturning moment (Nm)');
 box off
 legend(['V = ' num2str(v(vid)) ' m/s, Hs = ' num2str(hs(hsid)) ' m, Tp = ' num2str(tp(hs(hsid), 2))], 'box', 'off', 'location', 'eastoutside');
 
+subplot(5, 1, 5);
+plot(minutes(Time/60), squeeze(Ovr(vid, hsid, 3, :)))
+ylabel('Overturning moment (Nm)');
+box off
+legend(['V = ' num2str(v(vid)) ' m/s, Hs = ' num2str(hs(hsid)) ' m, Tp = ' num2str(tp(hs(hsid), 3))], 'box', 'off', 'location', 'eastoutside');
+
 exportgraphics(gcf, 'gfx/ValidtyCheckHs1.jpg') 
 
 % GEV parameters
-ks = zeros(gridSize) - 0.2;
+ks = zeros(gridSize) + SHAPE;
 sigmas = nan(gridSize);
 mus = nan(gridSize);
 
@@ -137,27 +142,29 @@ end
 % See: https://de.mathworks.com/help/stats/fitnlm.html
 % x(:, 1) = v, x(:, 2) = hs, x(:, 3) = tp
 modelfunSigma = @(b, x) (x(:,3) >= sqrt(2 * pi .* x(:,2) ./ (9.81 .* 1/14.99))) .* ...
-    (b(1) .* x(:,1).^2 + ...
-    (x(:,1) <= 25) .* (b(2) .* x(:,1) + b(3) .* x(:,1).^2) + ...
-    b(4) .* x(:,2).^2 ./ (1 + 0.005 .* (x(:,3) - 3).^2));
+    ((x(:,1) <= 25) .* (b(1) .* x(:,1) + b(2) .* x(:,1).^2) + ...
+    (x(:,1) > 25) .* b(3) + ...
+    b(4) .* x(:,2).^2 .* (1 + 0.3 ./ (1 + 5 .* (x(:,3) - 3).^2)));
 beta0 = [10^5 10^5 10^5 10^5];
 mdlSigma = fitnlm(X, ysigma, modelfunSigma, beta0, 'ErrorModel', 'proportional')
 sigmaHat = predict(mdlSigma, X);
 
 modelfunMu = @(b, x) (x(:,3) >= sqrt(2 * pi .* x(:,2) ./ (9.81 .* 1/14.99))) .* ...
-    (b(1) .* x(:,1).^2 + ...
-    (x(:,1) <= 25) .* (b(2) .* x(:,1) + b(3) .* x(:,1).^2) + ...
-    b(4) .* x(:,2).^1.3 ./ (1 + 0.005 .* (x(:,3) - 3).^2));
+    ((x(:,1) <= 25) .* (b(1) .* x(:,1) + b(2) .* x(:,1).^2 + ...
+    b(3) .* x(:,2).^1.0 .* (1 + 0.3 ./ (1 + 5 .* (x(:,3) - 3).^2))) + ...
+    (x(:,1) > 25 ) .* (b(4) .* x(:,1).^2 + ...
+    b(5) .* b(3) .* x(:,2).^1.0 .* (1 + 0.3 ./ (1 + 5 .* (x(:,3) - 3).^2))));
 % modelfunMu = @(b, x) (x(:,3) >= sqrt(2 * pi .* x(:,2) ./ (9.81 .* 1/14.99))) .* ...
 %     ((x(:,1) <= 11) .* b(1) .* x(:,1) + ...
 %     (x(:,1) > 11 & x(:,1) <= 13) .* b(1) .* 11 + ...
 %     (x(:,1) > 13 & x(:,1) <= 25) .* (b(1) .* 11 + b(2) .* (x(:,1) - 13) + b(3) .* (x(:,1) - 13).^2) + ...
 %     (x(:,1) > 25) .* b(4) .* x(:,1).^2 + ...
 %     b(5) .* x(:,2).^1.25 ./ (1 + 0.005 .* (x(:,3) - 3).^2));
-beta0 = [10^5 10^5 10^5 10^5];
+beta0 = [10^5 10^5 10^5 10^5 10^5];
 mdlMu = fitnlm(X, ymu, modelfunMu, beta0, 'ErrorModel', 'proportional')
 muHat = predict(mdlMu, X);
 
+% Sigma, mu over wind speed
 figure('Position', [100 100 900 500])
 t = tiledlayout(3, 4);
 for tpid = 1 : 4
@@ -167,15 +174,18 @@ for tpid = 1 : 4
     for i = 1 : 3
         X = [vv', zeros(length(vv), 1) + hs(i), zeros(length(vv), 1) + tp(hs(i), tpid)];
         hold on
-        plot(v, sigmas(i, :, tpid), 'o', 'color', colors{i});
-        labelhs = ['H_s = ' num2str(hs(i)) ' m'];
+        labelhs = ['H_s = ' num2str(hs(i)) ' m, from 1-hr simulation'];
+        plot(v, sigmas(i, :, tpid), 'o', 'color', colors{i}, 'DisplayName', labelhs);
+        labelhs = ['H_s = ' num2str(hs(i)) ' m, predicted'];
         plot(vv, predict(mdlSigma, X), 'color', colors{i}, 'DisplayName', labelhs);
     end
     xlabel('1-hr wind speed (m/s)');
     ylabel('\sigma (Nm)');
     title(['t_{p' num2str(tpid) '}']);
     if tpid == 1
-        legend('box', 'off');
+        lh = legend('box', 'off', 'Location','NorthOutside', ...
+            'Orientation', 'Horizontal', 'NumColumns', 2);
+        lh.Layout.Tile = 'North'; % <----- relative to tiledlayout
     end
 end
 
@@ -193,9 +203,6 @@ for tpid = 1 : 4
     xlabel('1-hr wind speed (m/s)');
     ylabel('\mu (Nm)');
     title(['t_{p' num2str(tpid) '}']);
-    if tpid == 1
-        legend('box', 'off');
-    end
 end
 
 for tpid = 1 : 4
@@ -212,10 +219,74 @@ for tpid = 1 : 4
     xlabel('1-hr wind speed (m/s)');
     ylabel('1-hr maximum moment (Nm)');
     title(['t_{p' num2str(tpid) '}']);
+end
+
+
+% Sigma, mu over hs
+figure('Position', [100 100 900 500])
+t = tiledlayout(3, 4);
+for tpid = 1 : 4
+    nexttile
+    hss = [0 : 0.1 : 15]';
+    colors = {'red', 'blue', 'black'};
+    countI = 1;
+    for i = [1, 8 15]
+        X = [zeros(length(hss), 1) + v(i), hss, tp(hss, tpid)];
+        hold on
+        labelv = ['V = ' num2str(v(i)) ' m/s, from 1-hr simulation'];
+        plot(hs, sigmas(:, i, tpid), 'o', 'color', colors{countI}, 'DisplayName', labelv);
+        labelv = ['V = ' num2str(v(i)) ' m/s, predicted'];
+        plot(hss, predict(mdlSigma, X), 'color', colors{countI}, 'DisplayName', labelv);
+        countI = countI + 1;
+    end
+    xlabel('Significant wave height (m)');
+    ylabel('\sigma (Nm)');
+    title(['t_{p' num2str(tpid) '}']);
     if tpid == 1
-        legend('box', 'off');
+        lh = legend('box', 'off', 'Location','NorthOutside', ...
+            'Orientation', 'Horizontal', 'NumColumns', 2);
+        lh.Layout.Tile = 'North'; % <----- relative to tiledlayout
     end
 end
+
+for tpid = 1 : 4
+    nexttile
+    hss = [0 : 0.1 : 15]';
+    colors = {'red', 'blue', 'black'};
+    countI = 1;
+    for i = [1, 8 15]
+        X = [zeros(length(hss), 1) + v(i), hss, tp(hss, tpid)];
+        hold on
+        labelv = ['V = ' num2str(v(i)) ' m/s, from 1-hr simulation'];
+        plot(hs, mus(:, i, tpid), 'o', 'color', colors{countI}, 'DisplayName', labelv);
+        labelv = ['V = ' num2str(v(i)) ' m/s, predicted'];
+        plot(hss, predict(mdlMu, X), 'color', colors{countI}, 'DisplayName', labelv);
+        countI = countI + 1;
+    end
+    xlabel('Significant wave height (m)');
+    ylabel('\mu (Nm)');
+    title(['t_{p' num2str(tpid) '}']);
+end
+
+for tpid = 1 : 4
+    nexttile
+    hss = [0 : 0.1 : 15]';
+    colors = {'red', 'blue', 'black'};
+    countI = 1;
+    for i = [1, 8 15]
+        X = [zeros(length(hss), 1) + v(i), hss, tp(hss, tpid)];
+        hold on
+        labelv = ['V = ' num2str(v(i)) ' m/s, from 1-hr simulation'];
+        plot(hs, maxr(i, :, tpid), 'o', 'color', colors{countI}, 'DisplayName', labelv);
+        %labelv = ['V = ' num2str(v(i)) ' m/s, predicted'];
+        %plot(hss, predict(mdlMu, X), 'color', colors{countI}, 'DisplayName', labelv);
+        countI = countI + 1;
+    end
+    xlabel('Significant wave height (m)');
+    ylabel('1-hr maximum moment (Nm)');
+    title(['t_{p' num2str(tpid) '}']);
+end
+
 
 % figure
 % nexttile
